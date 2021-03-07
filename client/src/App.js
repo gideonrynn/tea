9a1f6081e3f1ac71bb10a7ed9f0065d3185b77b7
Sarah Dillard <X5452@X5452.cable.rcn.com>
2021-03-06 20:32:38 -0600

    initial files, client side with  blurred background

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="home">
+          <h1>Tea</h1>
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const routes = require("./routes");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+require('dotenv').config({path:'.env'})
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => 
+    console.log(`🌎 ==> API server now on port ${PORT}!`)
+);
