3f6f43f3e4e689f454f20e3429cfd862dcb598ea
gideonrynn <sarah.c.dillard@gmail.com>
2021-08-31 11:42:44 -0500

    added backup data

--- scripts/queriesthemomentfortea.sql
+++ scripts/queriesthemomentfortea.sql
@@ -1,3 +1,14 @@
 use themomentfortea;
 
+-- ALTER TABLE themomentfortea.teas
+-- ADD COLUMN provider VARCHAR(255) AFTER background;
+
+-- ALTER TABLE themomentfortea.teas
+-- ADD COLUMN descriptionShort TEXT AFTER packetType,
+-- ADD COLUMN website TEXT AFTER provider,
+-- RENAME COLUMN description TO descriptionFull;
+
+-- select id, name, descriptionShort, descriptionFull
+-- from teas;
+
 SELECT * FROM themomentfortea.teas;
